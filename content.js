6ab8aec7978b7e05eb69af03c770dbcea46a29f9
Adam Smith <burkybang@gmail.com>
2020-05-20 13:10:00 -0400

    Bug fix: Didn't work on Mac

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  const disableCameraButton = document.querySelector('div[role="button"][aria-label="Turn off camera (ctrl + e)"]');
+  const disableCameraButton = document.querySelector('div[role="button"][aria-label^="Turn off camera ("]');
   if (disableCameraButton)
     disableCameraButton.click();
 };

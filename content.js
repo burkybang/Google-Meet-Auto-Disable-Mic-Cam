22fab76213a9258740368d352ee93ef1118c08d9
Adam Smith <burkybang@gmail.com>
2021-03-08 16:13:30 -0500

    Microsoft Edge compatibility

--- content.js
+++ content.js
@@ -10,59 +10,79 @@ const defaultSettings = {
   disableCam: true,
 };
 
+/**
+ * @typedef ButtonProps
+ * @property {string} label
+ * @property {string} key
+ * @property {string} storageName
+ * @property {'left'|'right'} direction
+ * @property {HTMLDivElement} element
+ */
+
+/** @type {ButtonProps[]} */
+const buttons = [
+  {
+    label: 'Microphone',
+    storageName: 'disableMic',
+    key: 'd',
+    direction: 'right',
+    element: null,
+  },
+  {
+    label: 'Camera',
+    storageName: 'disableCam',
+    key: 'e',
+    direction: 'left',
+    element: null,
+  },
+];
+
 /** @type {Promise<Settings>} */
 const settingsLoaded = new Promise(resolve => chrome.storage.sync.get(resolve));
 
 /** @type {Promise<void>} */
 const windowLoaded = new Promise(resolve => window.onload = () => resolve());
 
+/** @type {Promise<void>} */
+const buttonsLoaded = new Promise(async resolve => {
+  await windowLoaded;
+  
+  /** @type {MutationObserver} */
+  const observer = new MutationObserver(() => {
+    if (!buttons.every(button =>
+      button.element = document.body.querySelector(`div[role="button"][aria-label$=" + ${button.key})" i][data-is-muted]`),
+    )) return;
+    
+    observer.disconnect();
+    resolve();
+  });
+  
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+});
+
 Promise.all([
   settingsLoaded,
-  windowLoaded,
-]).then(([/**Settings*/settings = {}]) => {
+  buttonsLoaded,
+]).then(async ([/** Settings */ settings = {}]) => {
   settings = {...defaultSettings, ...settings};
   
-  /**
-   * @type {{
-   *   label: string,
-   *   buttonKey: string,
-   *   storageName: string,
-   *   direction: 'left'|'right',
-   * }[]}
-   */
-  const buttons = [
-    {
-      label: 'Microphone',
-      storageName: 'disableMic',
-      buttonKey: 'd',
-      direction: 'right',
-    },
-    {
-      label: 'Camera',
-      storageName: 'disableCam',
-      buttonKey: 'e',
-      direction: 'left',
-    },
-  ];
-  
-  buttons.forEach(({label, storageName, buttonKey, direction}) => {
-    
-    /** @type {HTMLDivElement} */
-    const button = document.querySelector(`div[role="button"][aria-label$=" + ${buttonKey})" i][data-is-muted]`);
-    if (!button) return;
+  buttons.forEach(({label, storageName, direction, element}) => {
     
     /** @type {boolean} */
     const autoDisable = settings[storageName] === true;
     
     /** @return {void} */
     const disable = () => {
-      if (button.dataset.isMuted === 'false')
-        button.click();
+      if (element.dataset.isMuted === 'false')
+        element.click();
     };
     
     /** @type {HTMLDivElement} */
-    const disableNode = document.createElement('div');
-    disableNode.innerHTML = `
+    const tempDivEl = document.createElement('div');
+    tempDivEl.innerHTML = `
       <label style="color:white; position:absolute; bottom:0; ${direction}:100px; z-index:1; cursor:pointer; white-space:nowrap;">
         <input type="checkbox" ${autoDisable ? 'checked' : ''} style="cursor:pointer; margin:0 4px 0 0; position:relative; top:1px;"/>
         <span>Auto Disable ${label}</span>
@@ -70,16 +90,16 @@ Promise.all([
     `;
     
     /** @type {HTMLInputElement} */
-    const checkbox = disableNode.querySelector('input');
-    checkbox.addEventListener('change', event => {
-      if (event.target.checked)
+    const checkboxEl = tempDivEl.querySelector('input');
+    checkboxEl.addEventListener('change', ({currentTarget}) => {
+      if (currentTarget.checked)
         disable();
       
-      settings[storageName] = event.target.checked;
+      settings[storageName] = currentTarget.checked;
       
       chrome.storage.sync.set(settings);
     });
-    button.parentElement.appendChild(disableNode.querySelector('label'));
+    element.parentElement.append(tempDivEl.children[0]);
     
     if (!autoDisable) return;
     

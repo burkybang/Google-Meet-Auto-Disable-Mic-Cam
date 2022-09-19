4ddf531efe54f74a708e54cf06ae1eda30e978e5
Adam Smith <burkybang@gmail.com>
2022-09-19 17:11:39 -0400

    Inline Meet logo SVG instead of using fetch

--- popup.ts
+++ popup.ts
@@ -1,7 +1,3 @@
-fetch('icon/logo.svg')
-  .then(response => response.text())
-  .then(svg => document.querySelector<HTMLDivElement>('#logo').innerHTML = svg);
-
 settingsLoaded.then(
   () => document.querySelector<HTMLDivElement>('#toggles')
     .append(...Object.values(createToggles()).map(toggle => toggle.labelEl)),

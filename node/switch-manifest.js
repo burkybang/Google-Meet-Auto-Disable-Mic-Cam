959cc8fe6f851d77a32579c73fc2e998c15ccb76
Adam Smith <burkybang@gmail.com>
2023-11-10 16:33:53 -0500

    switch-manifest: Update for latest node and TS

--- node/switch-manifest.js
+++ node/switch-manifest.js
@@ -1,17 +1,19 @@
-(() => {
-    try {
-        const fs = require('fs');
-        const path = require('path');
-        const [nodePath, scriptPath, manifestType] = process.argv;
-        if (!manifestType)
-            return console.log('No manifest type specified');
-        const manifestPath = path.join('..', `manifest_${manifestType}.json`);
-        if (!fs.existsSync(manifestPath))
-            return console.log('Manifest does not exist:', manifestPath);
-        fs.writeFileSync(path.join('..', 'manifest.json'), fs.readFileSync(manifestPath, { encoding: 'utf8' }), { encoding: 'utf8' });
-        console.log(`Manifest type ${manifestType} set`);
-    }
-    catch (e) {
-        console.log('e:', e);
-    }
-})();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const [nodePath, scriptPath, manifestType] = process.argv;
+if (!manifestType) {
+    console.log('No manifest type specified');
+    process.exit();
+}
+const manifestPath = path_1.default.join(path_1.default.parse(scriptPath).dir, '..', `manifest_${manifestType}.json`);
+if (!fs_1.default.existsSync(manifestPath)) {
+    console.log('Manifest does not exist:', manifestPath);
+    process.exit();
+}
+fs_1.default.writeFileSync(path_1.default.join('..', 'manifest.json'), fs_1.default.readFileSync(manifestPath, { encoding: 'utf8' }), { encoding: 'utf8' });
+console.log(`Manifest type ${manifestType} set`);
--- node/switch-manifest.ts
+++ node/switch-manifest.ts
@@ -4,24 +4,26 @@
  *   node switch-manifest firefox
  */
 
-((): void => {
-  try {
-    const fs = require('fs');
-    const path = require('path');
-    
-    const [nodePath, scriptPath, manifestType] = process.argv;
-    if (!manifestType)
-      return console.log('No manifest type specified');
-    
-    const manifestPath: string = path.join('..', `manifest_${manifestType}.json`);
-    
-    if (!fs.existsSync(manifestPath))
-      return console.log('Manifest does not exist:', manifestPath);
-    
-    fs.writeFileSync(path.join('..', 'manifest.json'), fs.readFileSync(manifestPath, {encoding: 'utf8'}), {encoding: 'utf8'});
-    console.log(`Manifest type ${manifestType} set`);
-    
-  } catch (e) {
-    console.log('e:', e);
-  }
-})();
+import fs from 'fs';
+import path from 'path';
+
+const [nodePath, scriptPath, manifestType] = process.argv;
+
+if (!manifestType) {
+  console.log('No manifest type specified');
+  process.exit();
+}
+
+const manifestPath: string = path.join(path.parse(scriptPath).dir, '..', `manifest_${manifestType}.json`);
+
+if (!fs.existsSync(manifestPath)) {
+  console.log('Manifest does not exist:', manifestPath);
+  process.exit();
+}
+
+fs.writeFileSync(
+  path.join('..', 'manifest.json'),
+  fs.readFileSync(manifestPath, {encoding: 'utf8'}),
+  {encoding: 'utf8'},
+);
+console.log(`Manifest type ${manifestType} set`);
